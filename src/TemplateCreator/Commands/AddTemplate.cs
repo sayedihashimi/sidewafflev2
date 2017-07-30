16eb589573d16d2259cd2a660875e95e67c6ae63
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-07-30 13:39:52 -0700

    initial commit of vs support

---
+++ src/TemplateCreator/Commands/AddTemplate.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.IO;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+
+namespace TemplateCreator
+{
+    internal sealed class AddTemplate
+    {
+        private readonly Package _package;
+        private Project _project;
+
+        private AddTemplate(Package package, OleMenuCommandService commandService)
+        {
+            _package = package;
+
+            var cmdId = new CommandID(PackageGuids.guidPackageCmdSet, PackageIds.AddTemplate);
+            var cmd = new OleMenuCommand(Execute, cmdId);
+            cmd.BeforeQueryStatus += BeforeQueryStatus;
+            commandService.AddCommand(cmd);
+        }
+
+        public static AddTemplate Instance
+        {
+            get; private set;
+        }
+
+        private IServiceProvider ServiceProvider
+        {
+            get { return _package; }
+        }
+
+        public static void Initialize(Package package, OleMenuCommandService commandService)
+        {
+            Instance = new AddTemplate(package, commandService);
+        }
+
+        private void BeforeQueryStatus(object sender, EventArgs e)
+        {
+            var button = (OleMenuCommand)sender;
+            button.Enabled = button.Visible = false;
+
+            _project = VsHelpers.DTE.SelectedItems.Item(1)?.Project;
+            string root = _project?.GetRootFolder();
+
+            if (string.IsNullOrEmpty(root))
+                return;
+
+            string templateFile = Path.Combine(root, Constants.Folder, Constants.TemplateFileName);
+
+            button.Enabled = button.Visible = !File.Exists(templateFile);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            string json = TemplateGenerator.CreateProjectTemplate(_project);
+
+            if (string.IsNullOrEmpty(json))
+            {
+                System.Diagnostics.Debug.Write("Could not generate the template");
+                return;
+            }
+
+            string root = _project.GetRootFolder();
+
+            string templateFile = Path.Combine(root, Constants.Folder, Constants.TemplateFileName);
+            string folder = Path.GetDirectoryName(templateFile);
+
+            Directory.CreateDirectory(folder);
+            File.WriteAllText(templateFile, json);
+
+            VsHelpers.OpenFileAndRefresh(templateFile);            
+        }
+    }
+}
---
+++ src/TemplateCreator/Shared/TemplateGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using EnvDTE;
+using Newtonsoft.Json.Linq;
+
+namespace TemplateCreator
+{
+    public class TemplateGenerator
+    {
+        public static string CreateProjectTemplate(Project proj)
+        {
+            string fullPath = proj.FullName;
+            string dir = Path.GetDirectoryName(fullPath);
+            string name = Path.GetFileNameWithoutExtension(fullPath);
+
+            var win = new InfoCollectorDialog(name);
+            win.CenterInVs();
+            if (win.ShowDialog().GetValueOrDefault())
+            {
+                const string solutionTemplate = @"{
+    ""author"": """",
+    ""classifications"": [ ],
+    ""description"": """",
+    ""name"": """",
+    ""defaultName"": """",
+    ""identity"": """",
+    ""groupIdentity"": """",
+    ""tags"": { },
+    ""shortName"": """",
+    ""sourceName"": """",
+    ""guids"": [ ]
+}";
+
+                var o = JObject.Parse(solutionTemplate);
+                o["author"] = win.AuthorTextBox.Text;
+                o["name"] = win.FriendlyNameTextBox.Text;
+                o["defaultName"] = win.DefaultNameTextBox.Text;
+                o["sourceName"] = Path.GetFileNameWithoutExtension(proj.FullName);
+                o["shortName"] = win.ShortNameTextBox.Text;
+
+                var guids = (JArray)o["guids"];
+                string projectGuid = ExtractProjectGuid(fullPath);
+
+                if (!string.IsNullOrEmpty(projectGuid))
+                {
+                    guids.Add(ExtractProjectGuid(fullPath));
+                }
+
+                return o.ToString();
+            }
+
+            return null;
+        }
+
+        private static string ExtractProjectGuid(string fullPath)
+        {
+            var doc = XDocument.Load(fullPath);
+            XElement element = doc.Descendants().FirstOrDefault(x => x.Name.LocalName == "ProjectGuid");
+            return element?.Value;
+        }
+    }
+}
